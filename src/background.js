2a1c2ff218bcad7db6f6158f51c247038650f0e5
Fei Yang <me@feiyang.moe>
2022-07-08 02:34:06 +0000

    feat: Translate with context menu.

--- src/background.js
+++ src/background.js
@@ -1,3 +1,33 @@
 import deepl from './utils/deepl.js'
 
-console.log('Hello World!')
+browser.contextMenus.create({
+  id: 'translate',
+  title: browser.i18n.getMessage('contextMenuTranslate'),
+  contexts: ['selection']
+})
+
+browser.contextMenus.onClicked.addListener(async (info, tab) => {
+  switch (info.menuItemId) {
+    case 'translate':
+      const o = await browser.storage.local.get(['free', 'token', 'target'])
+
+      try {
+        const r = await deepl(o.token, o.free, info.selectionText, o.target)
+
+        let t = ''
+        for (const i of r['translations']) {
+          t += i['text']
+        }
+  
+        browser.tabs.executeScript({
+          code: `alert(\`${t}\`)`
+        })
+      } catch (e) {
+        browser.runtime.openOptionsPage()
+      }
+
+
+
+      break
+  }
+})
