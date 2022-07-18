1a256be879f8f7e683d79e4b1a767859a9a20f3b
Fei Yang <me@feiyang.moe>
2022-07-18 07:38:44 +0000

    fix: Convert translation string to UTF-16 code to prevent unwanted code execution.

--- src/background.js
+++ src/background.js
@@ -12,21 +12,23 @@ browser.contextMenus.onClicked.addListener(async (info, tab) => {
       const o = await browser.storage.local.get(['free', 'token', 'target'])
 
       try {
-        const r = await deepl(o.token, o.free, info.selectionText, o.target)
-
-        let t = ''
-        for (const i of r['translations']) {
-          t += i['text']
-        }
-  
-        browser.tabs.executeScript({
-          code: `alert(\`${t}\`)`
-        })
+        var r = await deepl(o.token, o.free, info.selectionText, o.target)
       } catch (e) {
         browser.runtime.openOptionsPage()
+
+        break
       }
 
+      let t = ''
+      for (const i of r['translations']) {
+        t += i['text'].replace(/./g, (c) =>
+          c.charCodeAt(0).toString().padStart(5, '0')
+        )
+      }
 
+      browser.tabs.executeScript({
+        code: `alert('${t}'.replace(/.{5}/g, c => String.fromCharCode(parseInt(c))))`
+      })
 
       break
   }
